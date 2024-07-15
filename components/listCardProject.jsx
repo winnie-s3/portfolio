d4143c92e96a0d95d0a3b8a8199eb4bab3766c94
“Winnie <winniestefany303@gmail.com>
2024-07-15 18:41:03 -0300

    add project

--- components/listCardProject.jsx
+++ components/listCardProject.jsx
@@ -4,6 +4,7 @@ import { CardProject } from "./cardProjects";
 
 export function ListCardProject(){
   const projects = [
+    { projectName: "Rede Social Bloomie", projectDescription: "HTML, CSS, Javascript, PHP, MySQL, AJAX", projectImage: "https://github.com/user-attachments/assets/c8497de0-1298-4f2b-860c-02951ee4151a", projectLink: "https://www.redebloomie.com.br/" },
     { projectName: "Dashboard de vendas", projectDescription: "HTML, CSS, Javascript", projectImage: "https://github.com/winnie-s3/salon-booking/assets/74850021/c19553bf-60ef-46ac-a8af-7b3575d2521e", projectLink: "https://winnie-s3.github.io/dashboard-vendas/" },
     { projectName: "Salon Booking", projectDescription: "NodeJS, Express, MongoDB", projectImage: "https://github.com/winnie-s3/salon-booking/assets/74850021/3b071d1a-52a4-438d-a1c0-da8f5d0fc1ca", projectLink: "https://github.com/winnie-s3/salon-booking" },
     { projectName: "Rate Movies", projectDescription: "C#, .NET", projectImage: "https://user-images.githubusercontent.com/74850021/211688560-be8a08b6-e49e-458d-bc62-a6fd9dee77a2.png", projectLink: "https://github.com/winnie-s3/Rate-Movies" },
